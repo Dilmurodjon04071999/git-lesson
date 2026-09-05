aeab7ce930612b5d47383b1ea326bb4070517fc2
Dilmurod <dilmurodjonsotiboldiev703@gmail.com>
2026-09-05 17:33:31 +0500

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,14 @@ export default function Navbar() {
         <div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto error aliquam ut. Harum delectus vero nisi earum reiciendis praesentium voluptatum quas. Error earum labore id aut, animi numquam eligendi consequuntur exercitationem dolor quisquam eveniet nemo veritatis vel fugit dicta nesciunt dignissimos suscipit molestiae at iusto odio nisi excepturi totam voluptate. Voluptatibus, vero beatae quisquam in maiores repellat totam molestiae provident odio autem! Sequi consequuntur recusandae commodi sit veritatis voluptatem vitae fugiat debitis voluptates culpa quis, quibusdam consequatur iure dolorem et deserunt necessitatibus, nostrum voluptatum modi! Distinctio esse, voluptatum enim odit earum quo dolores laboriosam repellat repellendus ut quidem facilis culpa!</p>
         </div>
+        <div>
+          <ul>
+            <li>hello</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
     </div>
   )
 }
